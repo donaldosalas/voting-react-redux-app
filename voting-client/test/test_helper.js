92385ae6fa95d1247223abca55597ebe10f335f1
Donaldo S <DonaldoS@Donaldos-MacBook-Pro-2.local>
2016-09-08 11:35:57 -0700

    Install & setup immutableJS & chai-immutable

--- voting-client/test/test_helper.js
+++ voting-client/test/test_helper.js
@@ -1,4 +1,6 @@
 import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
@@ -17,3 +19,5 @@ Object.keys(window).forEach((key) => {
 		global[key] = window[key];
 	}
 });
+
+chai.use(chaiImmutable);
