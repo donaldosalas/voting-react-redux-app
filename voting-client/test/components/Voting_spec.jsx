a95b349b358f41c6876109483fb44a919f57a464
Donaldo S <DonaldoS@Donaldos-MacBook-Pro-2.local>
2016-09-08 14:09:27 -0700

    Add missing description to first test

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -10,7 +10,7 @@ import Voting from '../../src/components/Voting';
 
 describe('Voting', () => {
 
-	it('', () => {
+	it('renders a pair of buttons', () => {
 		const component = renderIntoDocument(
 			<Voting pair={ ["Trainspotting", "28 Days Later"] } />
 		);
