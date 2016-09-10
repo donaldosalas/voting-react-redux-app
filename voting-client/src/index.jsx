b2b0d97005b289c2daf8aa373b735f3396b9aedf
Donaldo S <DonaldoS@Donaldos-MacBook-Pro-2.local>
2016-09-10 14:00:40 -0700

    Connect client socket to server socket

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -15,15 +15,7 @@ import { VotingContainer } from './components/Voting';
 import { ResultsContainer } from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-	type: 'SET_STATE',
-	state: {
-		vote: {
-			pair: ['Sunshine', '28 Days Later'],
-			tally: { Sunshine: 2}
-		}
-	}
-});
+
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state => 
